3b515c1ec9ba01753036d09baf5e33d14a65ba4d
Priyanxxhiiii <priyanshishukla250604@gmail.com>
2025-07-18 10:13:10 +0530

    Add writable database and updated DB init script

--- init_database.py
+++ init_database.py
@@ -1,13 +1,20 @@
-metadata_obj=MetaData()
-todo_table=Table(
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, Boolean
+
+TABLE_NAME = "todo_table"
+engine = create_engine("sqlite:///todo_db.db")
+
+metadata_obj = MetaData()
+todo_table = Table(
     TABLE_NAME,
     metadata_obj,
-    Column("id", primary_key=True),
+    Column("id", Integer, primary_key=True),
     Column("title", String),
     Column("description", String),
     Column("created_at", Date),
     Column("due_at", Date),
     Column("done", Boolean),
 )
-if st.button("Create Database:"):
-    metadata_obj.create_all(bind=conn.engine)
+
+# Create the database and table
+metadata_obj.create_all(bind=engine)
+print("✅ Database and table created!")
